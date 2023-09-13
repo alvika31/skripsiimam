433d4a094133b95e614dd551985b4fe9236bb246
Alvika Prahasta <alvikaajiandrianty@gmail.com>
2023-09-13 22:18:30 +0700

    fix eror detail tepat waktu dan telat

--- application/views/absen/detailkehadiranPegawai.php
+++ application/views/absen/detailkehadiranPegawai.php
@@ -8,6 +8,7 @@
     <!-- Load Esri Leaflet Geocoder from CDN -->
     <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@3.1.3/dist/esri-leaflet-geocoder.css" integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g==" crossorigin="">
     <script src="https://unpkg.com/esri-leaflet-geocoder@3.1.3/dist/esri-leaflet-geocoder.js" integrity="sha512-mwRt9Y/qhSlNH3VWCNNHrCwquLLU+dTbmMxVud/GcnbXfOKJ35sznUmt3yM39cMlHR2sHbV9ymIpIMDpKg4kKw==" crossorigin=""></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/hosuaby/Leaflet.SmoothMarkerBouncing@v3.0.1/dist/bundle.js" crossorigin="anonymous"></script>
     <style>
         #map {
             height: 300px;
@@ -24,11 +25,10 @@
 
     <div class="container-fluid py-4">
         <div class="row" style="margin-bottom: 50px">
-            <a href="<?php echo site_url('absensi/dataKehadiran') ?>" class="btn btn-danger">Back</a>
+            <a href="<?php echo site_url('absensi/dataKehadiran_byUser') ?>" class="btn btn-danger">Back</a>
             <?php foreach ($user as $users) { ?>
                 <?php $tanggal = date('Y-m-d', strtotime($users->tgl_absen)); ?>
-                <div class="col">
-
+                <div class="col-12 col-md-6 mb-5">
 
                     <div class="card p-5">
                         <h5>Detail Presensi:</h5>
@@ -40,48 +40,25 @@
                         setlocale(LC_ALL, 'id-ID', 'id_ID');
                         ?>
                         <p>Tanggal Presensi: <?= tanggal_indo($tanggal, true) ?></p>
-                        <?php if ($users->keterangan_absen == 'Sakit') { ?>
-                            <p>Waktu Datang: <span class="badge badge-pill bg-gradient-danger">Sakit</span></p>
-                        <?php } elseif ($users->keterangan_absen == 'Cuti') { ?>
-                            <p>Waktu Datang: <span class="badge badge-pill bg-gradient-warning">Cuti</span></p>
 
-                        <?php } else { ?>
-                            <p>Waktu Datang: <?= $users->jam_absen ?></p>
-                        <?php } ?>
-                        <?php if ($users->keterangan_absen == 'Sakit') { ?>
-                            <p>Waktu Pulang: <span class="badge badge-pill bg-gradient-danger">Sakit</span></p>
-                        <?php } elseif ($users->keterangan_absen == 'Cuti') { ?>
-                            <p>Waktu Pulang: <span class="badge badge-pill bg-gradient-warning">Cuti</span></p>
-                        <?php } else { ?>
-                            <?php if ($users->jam_absen_pulang == '00:00:00') { ?>
-                                <p>Waktu Pulang: <span class="badge badge-pill bg-gradient-danger">Belum Presensi Pulang</span></p>
+                        <p>Waktu Datang: <?= $users->jam_absen ?></p>
+                        <p>Keterangan:
+                            <?php if ($users->status_absen == 0) { ?>
+                                <span class="badge bg-gradient-danger">Telat</span>
                             <?php } else { ?>
-                                <p>Waktu Pulang: <?= $users->jam_absen_pulang ?></p>
-                        <?php }
-                        } ?>
-                        <p>Keterangan Presensi: <?= $users->keterangan_absen ?></p>
+                                <span class="badge bg-gradient-success">Tepat Waktu</span>
+                            <?php } ?>
 
-
+                        </p>
                     </div>
                 </div>
-
-
-                 <div class="col-12 col-md-6">
-                    <div class="card p-0">
-                        <h5 class="p-4">Foto Selfie:</h5>
-                    </div></div>
-
-
-
-
-
-
-
                 <div class="col-12 col-md-6">
                     <div class="card p-0">
+
                         <h5 class="p-4">Maps Presensi:</h5>
 
                         <div id="map"> </div>
+
                         <script>
                             setLocation = [<?= $users->lat_absen ?>, <?= $users->long_absen ?>]
                             var map = L.map('map').setView([<?= $users->lat_absen ?>, <?= $users->long_absen ?>], 13);
@@ -89,6 +66,16 @@
                             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                                 attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
                             }).addTo(map);
+
+                            var geocodeService = L.esri.Geocoding.geocodeService({
+                                apikey: 'AAPK1e6d5c2dfd0648f8bc8379c6d8af7696XVzE0quYtPh_8bOmEz-grWLy69WPhunJ32NkS-ULj8OFOJ6J0WvmP8mfvlVgdmIg' // replace with your api key - https://developers.arcgis.com
+                            });
+
+                            var greenIcon = L.icon({
+                                iconUrl: '<?= base_url('upload/marker.png') ?>',
+
+                                iconSize: [50], // size of the icon
+                            });
 
                             var office = L.icon({
                                 iconUrl: '<?= base_url('upload/office.png') ?>',
@@ -105,16 +92,6 @@
                                 'width': '100',
                                 'className': 'popupCustom'
                             }
-
-                            var geocodeService = L.esri.Geocoding.geocodeService({
-                                apikey: 'AAPK1e6d5c2dfd0648f8bc8379c6d8af7696XVzE0quYtPh_8bOmEz-grWLy69WPhunJ32NkS-ULj8OFOJ6J0WvmP8mfvlVgdmIg' // replace with your api key - https://developers.arcgis.com
-                            });
-
-                            var greenIcon = L.icon({
-                                iconUrl: '<?= base_url('upload/marker.png') ?>',
-
-                                iconSize: [50], // size of the icon
-                            });
                             geocodeService.reverse().latlng(setLocation).run(function(error, result) {
                                 if (error) {
                                     return;
@@ -124,12 +101,26 @@
                                     icon: greenIcon
                                 }).addTo(map).bindPopup(result.address.Match_addr, customOptions).openPopup();
                             });
-                         
+                            var customPopup = "Office: <br/>Komplek PU Blok B1 No.10, Buah Batu (Cipagalo Cipagalo Bojongsoang, Kujangsari, Kec. Bandung Kidul, Kota Bandung, Jawa Barat 40287";
+                            L.marker([-6.964827252728149, 107.63882345223466], {
+                                icon: office
+                            }).addTo(map).bindPopup(customPopup, customOptions2);
                         </script>
 
 
                     </div>
                 </div>
+                <div class="col-12 col-md-6">
+                    <div class="card p-0">
+                        <h5 class="p-4">Foto Selfie:</h5>
+                        <img src="<?= base_url() . '/selfie_karyawan/' . $users->selfie_absen ?>" width="100%" alt="">
+                    </div>
+                </div>
+
+
+
+
+
 
         </div>
     </div>
